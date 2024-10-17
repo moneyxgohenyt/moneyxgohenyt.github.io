67ec7949fc43756844dd12b1093d2d230a24ab89
JamalW <118034658+moneyxgohenyt@users.noreply.github.com>
2024-10-17 00:55:19 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -39,7 +39,7 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-
+    createPlatform(70, 90, canvas.width + 150, 78);
 
     
     // TODO 2
