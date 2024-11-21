9643e0cd920c4219444edc113a51308330dbccb2
JamalW <118034658+moneyxgohenyt@users.noreply.github.com>
2024-11-21 00:46:50 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -16,10 +16,10 @@ $(function () {
       setInterval(main, 1000 / frameRate);
     }
     // Create walls - do not delete or modify this code
-    createPlatform(70, 90, canvas.width + 150, 78); //top
-    createPlatform(50, canvas.height  10, canvas.width + 100, 200); //right
-    createPlatform(50, 50, 50, canvas.height + 500); //bottom
-    createPlatform(canvas.width, 50, 50, canvas.height + 100);
+    createPlatform(70, 0, canvas.width + 15, 7); //top
+    createPlatform(50, canvas.height + 10 ,canvas.width + 100, 200); //right
+    createPlatform(5, 0, 5, canvas.height + 5); //bottom
+    createPlatform(canvas.width, 50, 50, canvas.height + 100); //left
 
     /**
      * Uncomment the drawGrid() function call below to add a "grid" to your platformer game's screen
@@ -39,17 +39,25 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-    createPlatform(500, 300, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
-
-    
+    createPlatform(5, 750, 10000000000, 1); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    createPlatform(400, 555, 200, 20);
+    createPlatform(200, 555, 200, 20);
+    createPlatform(100, 555, 200, 20);
+    createPlatform(100, 0, 10, 600);
+    createPlatform(100, 400, 200, 20);
+    createPlatform(400, 400, 200, 20);
+    createPlatform(700, 400, 200, 20);
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-    reateCollectable("steve", 200, 170, 6, 0.7)
-
+    createCollectable("steve", 200, 170, 6, 0.7)
+    createCollectable("max", 500, 300, 20, 0.5); // creates a "steve" collectible at the coordinates (500, 300), falling with a high gravity of 20, and bouncing with 50% bounce
+    createCollectable("grace", 500, 300); // creates a "grace" collectible at the coordinates (500, 300), falling with default gravity and bouncing with default bounce %
+    createCollectable("database", 500, 300, 20, 0.5); // creates a "steve" collectible at the coordinates (500, 300), falling with a high gravity of 20, and bouncing with 50% bounce
+    createCollectable("kennedi", 500, 300); // creates a "grace" collectible at the coordinates (500, 300), falling with default gravity and bouncing with default bounce %
     
     // TODO 3
     // Create cannons
@@ -57,8 +65,11 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-
-
+    createCannon("right", 500, 1000); // cannon on right wall, 600px down, shooting once per second
+    createCannon("left", 600, 1000); // cannon on left wall, 600px down, shooting once per second
+    createCannon("right", 50, 900); // cannon on right wall, 600px down, shooting once per second
+    createCannon("top", 600, 1000); // cannon on top wall, 600px down, shooting once per second
+    createCannon("left", 500, 1000)
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
--- projects/platformer/setup.js
+++ projects/platformer/setup.js
@@ -1,9 +1,9 @@
 // setup variables
-const walkAcceleration = 7; // how much is added to the speed each frame
-const gravity = 100; // how much is subtracted from speedY each frame
+const walkAcceleration = 10; // how much is added to the speed each frame
+const gravity = 1; // how much is subtracted from speedY each frame
 const friction = 1; // how much the player is slowed each frame
 const maxSpeed = 10; // maximum horizontal speed, not vertical
-const playerJumpStrength = 100; // this is subtracted from the speedY each jump
+const playerJumpStrength = 20; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
 
 /////////////////////////////////////////////////
